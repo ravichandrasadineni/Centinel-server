715ef8f98d239bf3ec37e1e10ba615099a0c6bcb
ravichandrasadineni <ravichandrasadineni@gmail.com>
2015-01-12 21:46:18 -0500

    using node-generator

--- app.js
+++ app.js
@@ -1,33 +1,60 @@
-    /**
-    * Created by ravichandrasadineni on 1/9/15.
-    */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-    var http = require('http');
-    var fs = require('fs');
-    var express = require('express');
-    var iniparser = require('iniparser');
-    var moment = require('moment');
-    var app = express();
+var app = express();
 
-    app.use(express.logger({
-        format: ':date :remote-addr :url :method :status :response-time',
-        stream : fs.createWriteStream('./Logs/' + moment(Date.now()).format("YYYY-MM-DD HH:mm").toString() + '.log', {'flags': 'w'})
-    }));
-    app.set("view engine", 'jade');
-    app.set('views', './views');
-    app.use(express.static('./public'));
-    app.use(express.static('./files'));
-    app.use(express.static('./downloads'));
-    app.use(app.router);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    app.use(express.errorHandler());
-    var config = iniparser.parseSync('./config.ini');
-    var routes = require('./routes')(app);
-    http.createServer(app).listen(config.port, function() {
-        console.log('Express app started');
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 
-
+module.exports = app;
